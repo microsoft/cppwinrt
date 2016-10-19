6427f691a680eb59951cd2e589f2e8f5e5fc0635
Richard <legalize@xmission.com>
2016-10-19 16:35:31 -0600

    Use / for include paths, not \

--- 10.0.14393.0/Samples/Video/pch.h
+++ 10.0.14393.0/Samples/Video/pch.h
@@ -2,11 +2,11 @@
 
 #pragma comment(lib, "windowsapp") 
 
-#include "winrt\Windows.ApplicationModel.Core.h"
-#include "winrt\Windows.Foundation.h"
-#include "winrt\Windows.UI.Core.h"
-#include "winrt\Windows.UI.Composition.h"
-#include "winrt\Windows.Media.Core.h"
-#include "winrt\Windows.Media.Playback.h"
-#include "winrt\Windows.Storage.Pickers.h"
-#include "winrt\Windows.Graphics.Imaging.h"
+#include "winrt/Windows.ApplicationModel.Core.h"
+#include "winrt/Windows.Foundation.h"
+#include "winrt/Windows.UI.Core.h"
+#include "winrt/Windows.UI.Composition.h"
+#include "winrt/Windows.Media.Core.h"
+#include "winrt/Windows.Media.Playback.h"
+#include "winrt/Windows.Storage.Pickers.h"
+#include "winrt/Windows.Graphics.Imaging.h"
