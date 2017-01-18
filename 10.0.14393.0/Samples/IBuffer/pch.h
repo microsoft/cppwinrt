772fb4b4d174ad0e73fb67e744d6f5f671c1d9dd
Kevin Welton <kevin.welton@microsoft.com>
2017-01-18 13:46:11 -0800

    Add IBuffer sample

---
+++ 10.0.14393.0/Samples/IBuffer/pch.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#pragma comment(lib, "windowsapp") 
+
+#include "winrt/Windows.Storage.Pickers.h"
+#include "winrt/Windows.Storage.Streams.h"
+#include "winrt/Windows.UI.Core.h"
+#include "winrt/Windows.UI.Xaml.Controls.h"
+#include "winrt/Windows.UI.Xaml.Media.Imaging.h"
+#include "winrt/Windows.Graphics.Imaging.h"
+
+#include <robuffer.h>
+#include <future>
