a88c83bd7bb42a420288263acd6ed73421f2057e
Johan Lindfors <johan.lindfors@coderox.se>
2017-01-30 10:23:20 +0100

    Initial solution of the DirectX sample

---
+++ 10.0.14393.0/Samples/DirectX/App.cpp
@@ -0,0 +1,171 @@
+﻿#include "pch.h"
+#include "Common\DeviceResources.h"
+#include "Main.h"
+
+using namespace winrt;
+using namespace winrt::Windows::ApplicationModel;
+using namespace winrt::Windows::ApplicationModel::Core;
+using namespace winrt::Windows::UI::Core;
+using namespace winrt::Windows::UI::Composition;
+using namespace winrt::Windows::Foundation;
+using namespace winrt::Windows::Foundation::Collections;
+using namespace winrt::Windows::Graphics::Display;
+
+WINRT_EXPORT namespace DirectX {
+
+	struct App : implements<App, IFrameworkViewSource, IFrameworkView>
+	{
+		IFrameworkView CreateView()
+		{
+			return *this;
+		}
+
+		void Initialize(CoreApplicationView const & applicationView)
+		{
+			applicationView.Activated({ this, &App::OnActivated });
+
+			CoreApplication::Suspending({ this, &App::OnSuspending });
+
+			CoreApplication::Resuming({ this, &App::OnResuming });
+
+			m_deviceResources = std::make_shared<DX::DeviceResources>();
+		}
+
+		void Load(hstring_ref)
+		{
+			if (m_main == nullptr)
+			{
+				m_main = std::unique_ptr<Main>(new Main(m_deviceResources));
+			}
+		}
+
+
+		void SetWindow(CoreWindow const & window)
+		{
+			window.SizeChanged({ this, &App::OnWindowSizeChanged });
+
+			window.VisibilityChanged({ this, &App::OnVisibilityChanged });
+
+			window.Closed({ this, &App::OnWindowClosed });
+
+			DisplayInformation currentDisplayInformation = DisplayInformation::GetForCurrentView();
+
+			currentDisplayInformation.DpiChanged({ this, &App::OnDpiChanged });
+
+			currentDisplayInformation.OrientationChanged({ this, &App::OnOrientationChanged });
+
+			DisplayInformation::DisplayContentsInvalidated({ this, &App::OnDisplayContentsInvalidated });
+
+			m_deviceResources->SetWindow(CoreWindow::GetForCurrentThread());
+		}
+
+		void Run()
+		{
+			while (!m_windowClosed)
+			{
+				if (m_windowVisible)
+				{
+					CoreWindow::GetForCurrentThread().Dispatcher().ProcessEvents(CoreProcessEventsOption::ProcessAllIfPresent);
+
+					m_main->Update();
+
+					if (m_main->Render())
+					{
+						m_deviceResources->Present();
+					}
+				}
+				else
+				{
+					CoreWindow::GetForCurrentThread().Dispatcher().ProcessEvents(CoreProcessEventsOption::ProcessOneAndAllPending);
+				}
+			}
+		}
+
+		void Uninitialize()
+		{
+		}
+
+
+	private:
+
+		void OnActivated(IInspectable const&, IInspectable const&) {
+			CoreWindow::GetForCurrentThread().Activate();
+		}
+
+		void App::OnSuspending(IInspectable const&, SuspendingEventArgs const& args)
+		{
+			// Save app state asynchronously after requesting a deferral. Holding a deferral
+			// indicates that the application is busy performing suspending operations. Be
+			// aware that a deferral may not be held indefinitely. After about five seconds,
+			// the app will be forced to exit.
+			SuspendingDeferral deferral = args.SuspendingOperation().GetDeferral();
+
+			concurrency::create_task([this, deferral]()
+			{
+				m_deviceResources->Trim();
+
+				// Insert your code here.
+
+				deferral.Complete();
+			});
+		}
+
+		void App::OnResuming(IInspectable const&, IInspectable const&)
+		{
+			// Restore any data or state that was unloaded on suspend. By default, data
+			// and state are persisted when resuming from suspend. Note that this event
+			// does not occur if the app was previously terminated.
+
+			// Insert your code here.
+		}
+
+		void App::OnWindowSizeChanged(CoreWindow const& sender, IInspectable const&)
+		{
+			m_deviceResources->SetLogicalSize(Size(sender.Bounds().Width, sender.Bounds().Height));
+			m_main->CreateWindowSizeDependentResources();
+		}
+
+		void App::OnVisibilityChanged(IInspectable const&, VisibilityChangedEventArgs const& args)
+		{
+			m_windowVisible = args.Visible();
+		}
+
+		void App::OnWindowClosed(IInspectable const&, IInspectable const&)
+		{
+			m_windowClosed = true;
+		}
+
+		// DisplayInformation event handlers.
+
+		void App::OnDpiChanged(DisplayInformation const& sender, IInspectable const&)
+		{
+			// Note: The value for LogicalDpi retrieved here may not match the effective DPI of the app
+			// if it is being scaled for high resolution devices. Once the DPI is set on DeviceResources,
+			// you should always retrieve it using the GetDpi method.
+			// See DeviceResources.cpp for more details.
+			m_deviceResources->SetDpi(sender.LogicalDpi());
+			m_main->CreateWindowSizeDependentResources();
+		}
+
+		void App::OnOrientationChanged(DisplayInformation const& sender, IInspectable const&)
+		{
+			m_deviceResources->SetCurrentOrientation(sender.CurrentOrientation());
+			m_main->CreateWindowSizeDependentResources();
+		}
+
+		void App::OnDisplayContentsInvalidated(IInspectable const&, IInspectable const&)
+		{
+			m_deviceResources->ValidateDevice();
+		}
+
+		std::shared_ptr<DX::DeviceResources> m_deviceResources;
+		std::unique_ptr<Main> m_main;
+		bool m_windowClosed;
+		bool m_windowVisible;
+	};
+}
+
+int __stdcall wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
+{
+	CoreApplication::Run(DirectX::App());
+}
---
+++ 10.0.14393.0/Samples/DirectX/Common/DirectXHelper.h
@@ -0,0 +1,59 @@
+﻿#pragma once
+
+#include <ppltasks.h>	// For create_task
+#include <future>
+
+using namespace winrt::Windows::Foundation;
+using namespace winrt::Windows::Storage;
+using namespace winrt::Windows::Storage::Streams;
+
+namespace DX
+{
+	inline void ThrowIfFailed(HRESULT hr)
+	{
+		if (FAILED(hr))
+		{
+			// Set a breakpoint on this line to catch Win32 API errors.
+			throw winrt::hresult_error(hr);
+		}
+	}
+
+	inline std::future<std::vector<byte>> ReadDataAsync(const std::wstring& filename)
+	{
+		auto folder = winrt::Windows::ApplicationModel::Package::Current().InstalledLocation();
+		auto file = co_await folder.GetFileAsync(winrt::hstring_ref(filename.c_str()));
+		IBuffer fileBuffer = co_await FileIO::ReadBufferAsync(file);
+		std::vector<byte> returnBuffer;
+		returnBuffer.resize(fileBuffer.Length());
+		DataReader::FromBuffer(fileBuffer).ReadBytes(winrt::array_ref<byte>(returnBuffer));
+		return returnBuffer;
+	}
+
+	// Converts a length in device-independent pixels (DIPs) to a length in physical pixels.
+	inline float ConvertDipsToPixels(float dips, float dpi)
+	{
+		static const float dipsPerInch = 96.0f;
+		return floorf(dips * dpi / dipsPerInch + 0.5f); // Round to nearest integer.
+	}
+
+#if defined(_DEBUG)
+	// Check for SDK Layer support.
+	inline bool SdkLayersAvailable()
+	{
+		HRESULT hr = D3D11CreateDevice(
+			nullptr,
+			D3D_DRIVER_TYPE_NULL,       // There is no need to create a real hardware device.
+			0,
+			D3D11_CREATE_DEVICE_DEBUG,  // Check for the SDK layers.
+			nullptr,                    // Any feature level will do.
+			0,
+			D3D11_SDK_VERSION,          // Always set this to D3D11_SDK_VERSION for Windows Store apps.
+			nullptr,                    // No need to keep the D3D device reference.
+			nullptr,                    // No need to know the feature level.
+			nullptr                     // No need to keep the D3D device context reference.
+		);
+
+		return SUCCEEDED(hr);
+	}
+#endif
+}
