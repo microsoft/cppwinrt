67d9e8829aacb098f932b3c35467b7323919d838
Kenny Kerr <kekerr@microsoft.com>
2019-10-17 09:26:14 -0700

    build

--- build_tools.cmd
+++ build_tools.cmd
@@ -8,6 +8,11 @@ if "%target_platform%"=="" set target_platform=x64
 if "%target_configuration%"=="" set target_configuration=Release
 if "%target_version%"=="" set target_version=1.2.3.4
 
+call msbuild /m /p:Configuration=%target_configuration%,Platform=%target_platform%,CppWinRTBuildVersion=%target_version% cppwinrt.sln /t:fast_fwd
+
+if "%target_platform%"=="ARM" goto :exit
+if "%target_platform%"=="ARM64" goto :exit
+
 call nuget restore cppwinrt.sln
 call msbuild /m /p:Configuration=%target_configuration%,Platform=%target_platform%,CppWinRTBuildVersion=%target_version% cppwinrt.sln /t:cppwinrt
 call build_projection %target_platform% %target_configuration%
