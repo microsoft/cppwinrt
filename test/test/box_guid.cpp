c4fe5e60d73b16e95b6ebd7c022a09f687dd70ee
Kenny Kerr <kekerr@microsoft.com>
2019-11-15 08:17:34 -0800

    guid

--- test/test/box_guid.cpp
+++ test/test/box_guid.cpp
@@ -8,14 +8,14 @@ TEST_CASE("box_guid")
     auto box_a = winrt::box_value(winrt_guid);
     auto box_b = winrt::box_value(sdk_guid);
 
-    auto unbox_a = winrt::unbox_value<winrt::guid>(box_a);
-    auto unbox_b = winrt::unbox_value<GUID>(box_a);
+    winrt::guid unbox_a = winrt::unbox_value<winrt::guid>(box_a);
+    GUID unbox_b = winrt::unbox_value<GUID>(box_a);
 
     REQUIRE(unbox_a == winrt_guid);
     REQUIRE(unbox_b == sdk_guid);
 
-    unbox_a = winrt::unbox_value_or<winrt::guid>(box_a, {});
-    unbox_b = winrt::unbox_value_or<GUID>(box_a, {});
+    unbox_a = winrt::unbox_value_or<winrt::guid>(box_a, winrt::guid{});
+    unbox_b = winrt::unbox_value_or<GUID>(box_a, GUID{});
 
     REQUIRE(unbox_a == winrt_guid);
     REQUIRE(unbox_b == sdk_guid);
