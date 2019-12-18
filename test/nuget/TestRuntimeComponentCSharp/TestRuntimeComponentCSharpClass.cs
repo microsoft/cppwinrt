add7eec738996840caccf3ad95ccf96c121011bf
Johan Laanstra <jlaans@microsoft.com>
2019-12-18 12:02:11 -0800

    Add a C# runtime component to the test solution. (#448)

---
+++ test/nuget/TestRuntimeComponentCSharp/TestRuntimeComponentCSharpClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRuntimeComponentCSharp
+{
+    public sealed class TestRuntimeComponentCSharpClass
+    {
+        public void Test()
+        {
+        }
+    }
+}
