c632231192798ad3ab8dc5cb0b016074063a7c85
Johan Laanstra <jlaans@microsoft.com>
2020-01-29 21:32:22 -0800

    Update nuget.exe url to point to the right location. (#507)

--- build_test_all.cmd
+++ build_test_all.cmd
@@ -9,7 +9,7 @@ if "%target_configuration%"=="" set target_configuration=Release
 if "%target_version%"=="" set target_version=1.2.3.4
 
 if not exist ".\.nuget" mkdir ".\.nuget"
-if not exist ".\.nuget\nuget.exe" powershell -Command "Invoke-WebRequest https://www.nuget.org/nuget.exe -OutFile .\.nuget\nuget.exe"
+if not exist ".\.nuget\nuget.exe" powershell -Command "Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile .\.nuget\nuget.exe"
 
 call .nuget\nuget.exe restore cppwinrt.sln"
 call .nuget\nuget.exe restore natvis\cppwinrtvisualizer.sln
