cdb87386348ec4b1d6289701d12f23d4bc80db10
Kenny Kerr <kenny.kerr@microsoft.com>
2020-03-03 07:46:31 -0800

    data

--- test/test/hstring_empty.cpp
+++ test/test/hstring_empty.cpp
@@ -13,4 +13,12 @@ TEST_CASE("hstring_empty")
     // Using wcslen to both validate that the strings are empty *and* that they are not null.
     REQUIRE(wcslen(s.c_str()) == 0);
     REQUIRE(wcslen(s.data()) == 0);
+
+    std::wstring_view v = s;
+    REQUIRE(v.empty());
+    REQUIRE(v.size() == 0);
+    REQUIRE(v == L""sv);
+
+    // Some existing code relies on data returning a valid string.
+    REQUIRE(v.data() == L""sv);
 }
