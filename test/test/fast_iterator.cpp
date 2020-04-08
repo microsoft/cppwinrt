f3a2779e661b6823b311c081d58e085c184f1d03
Raymond Chen <oldnewthing@users.noreply.github.com>
2020-04-08 12:36:44 -0700

    Random-access iterators over IVector/IVectorView (#579)

--- test/test/fast_iterator.cpp
+++ test/test/fast_iterator.cpp
@@ -3,6 +3,7 @@
 TEST_CASE("fast_iterator")
 {
     {
+        // Forward iteration.
         auto v = winrt::single_threaded_vector<int>({ 1, 2, 3 });
 
         std::vector<int> result;
@@ -12,6 +13,7 @@ TEST_CASE("fast_iterator")
         REQUIRE((result == std::vector{ 1, 2, 3 }));
     }
     {
+        // Reverse iteration.
         auto v = winrt::single_threaded_vector<int>({ 1, 2, 3 });
 
         std::vector<int> result;
@@ -19,5 +21,31 @@ TEST_CASE("fast_iterator")
         std::copy(rbegin(v), rend(v), std::back_inserter(result));
 
         REQUIRE((result == std::vector{ 3, 2, 1 }));
+    }
+    {
+        // Value-initialization.
+        auto v = winrt::single_threaded_vector<int>({ 9, 5, 4, 1, 1, 3 });
+        decltype(begin(v)) dummy;
+        decltype(begin(v)) dummy2;
+        // Must be able to default-construct the iterator,
+        // but the only thing you can do with it is compare
+        // with another one.
+        REQUIRE(dummy == dummy2);
+    }
+    {
+        // Read-only random access.
+        auto v = winrt::single_threaded_vector<int>({ 9, 5, 4, 1, 1, 3 });
+        REQUIRE(std::is_heap(begin(v), end(v)));
+
+        auto vbegin = begin(v);
+        auto value = *vbegin++;
+        REQUIRE(value == 9);
+        value = *--vbegin;
+        REQUIRE(value == 9);
+        REQUIRE(vbegin[2] == 4);
+        REQUIRE(vbegin + 2 > vbegin);
+        REQUIRE(vbegin < vbegin + 2);
+        REQUIRE(vbegin + 2 - 2 == vbegin);
+        REQUIRE(end(v) - begin(v) == v.Size());
     }
 }
