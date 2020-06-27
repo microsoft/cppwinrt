a89988d11370bdaa9132b72169a9a7753492e66b
Raymond Chen <oldnewthing@users.noreply.github.com>
2020-06-26 23:26:10 -0700

    Don't destroy the IAsyncInfo from inside Completed handler
    If the Completed handler runs synchronously, that would
    cause us to destroy the operation while a method on
    the operation is still running. That's not very nice.
    
    This stems from the fact that synchronous completion
    causes the coroutine to resume before put_Completed
    returns, and resuming the coroutine causes it to get
    the result and then destroy the async operation.
    To avoid this problem, we extend the lifetime of the
    async operation inside await_suspend, so that the
    synchronous completion doesn't destruct the async operation
    until after put_Completed has returned.
    
    If the operation completes asynchronously, then the
    destruction happens at coroutine resumption, which is fine.
    The put_Completed method returned a long time ago.
    
    We cannot "steal" the async operation into await_suspend,
    because await_resume needs the operation to get the results.
    We have to make a copy. This costs us an AddRef/Release pair,
    but avoiding the AddRef/Release will cost us a few atomic
    operations (and memory barriers), so it's basically a wash.

--- strings/base_coroutine_foundation.h
+++ strings/base_coroutine_foundation.h
@@ -140,6 +140,7 @@ namespace winrt::impl
 
         void await_suspend(std::experimental::coroutine_handle<> handle)
         {
+            auto extend_lifetime = async;
             async.Completed([this, handler = disconnect_aware_handler{ handle }](auto&&, auto operation_status) mutable
             {
                 status = operation_status;
