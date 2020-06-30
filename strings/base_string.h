7d7cc55ec7a6dbb9ce3a93594993d7c00e4d0a3f
Raymond Chen <oldnewthing@users.noreply.github.com>
2020-06-30 10:47:57 -0700

    Move zero-test to concat_hstring

--- strings/base_string.h
+++ strings/base_string.h
@@ -423,13 +423,13 @@ namespace winrt::impl
         hstring_builder& operator=(hstring_builder const&) = delete;
 
         explicit hstring_builder(uint32_t const size) :
-            m_handle(size ? impl::precreate_hstring_on_heap(size) : nullptr)
+            m_handle(impl::precreate_hstring_on_heap(size))
         {
         }
 
         wchar_t* data() noexcept
         {
-            return m_handle ? const_cast<wchar_t*>(m_handle.get()->ptr) : reinterpret_cast<wchar_t*>(this);
+            return const_cast<wchar_t*>(m_handle.get()->ptr);
         }
 
         hstring to_hstring()
