5a5e33ce6b613ad209a10931684c753a3267324a
Raymond Chen <oldnewthing@users.noreply.github.com>
2020-07-10 08:24:02 -0700

    Disable warning 4458 in delegates (#686)

--- test/test/pch.h
+++ test/test/pch.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#pragma warning(4: 4458) // ensure we compile clean with this warning enabled
+
 #define WINRT_LEAN_AND_MEAN
 #include <unknwn.h>
 #include "winrt/Windows.Foundation.Collections.h"
