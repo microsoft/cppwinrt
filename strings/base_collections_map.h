d1fd32490e507b590b1a4392c11808541d4ba595
Johan Laanstra <jlaans@microsoft.com>
2020-09-14 14:51:11 -0700

    Fix return type.

--- strings/base_collections_map.h
+++ strings/base_collections_map.h
@@ -44,13 +44,13 @@ namespace winrt::impl
 WINRT_EXPORT namespace winrt
 {
     template <typename K, typename V, typename Compare = std::less<K>, typename Allocator = std::allocator<std::pair<K const, V>>>
-    Windows::Foundation::Collections::IMap<K, V> map_view()
+    Windows::Foundation::Collections::IMapView<K, V> map_view()
     {
         return make<impl::input_map_view<K, V, std::map<K, V, Compare, Allocator>>>(std::map<K, V, Compare, Allocator>{});
     }
 
     template <typename K, typename V, typename Compare = std::less<K>, typename Allocator = std::allocator<std::pair<K const, V>>>
-    Windows::Foundation::Collections::IMap<K, V> map_view(std::map<K, V, Compare, Allocator>&& values)
+    Windows::Foundation::Collections::IMapView<K, V> map_view(std::map<K, V, Compare, Allocator>&& values)
     {
         return make<impl::input_map_view<K, V, std::map<K, V, Compare, Allocator>>>(std::move(values));
     }
