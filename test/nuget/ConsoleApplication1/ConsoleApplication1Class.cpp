2a7f7236940d53bfab07ecb1fb6fd1771c2fc3ad
Johan Laanstra <jlaans@microsoft.com>
2020-11-13 15:08:00 -0800

    Remove the use of TargetPlatformWinMDLocation as the C++ build targets put a problematic path in it.

---
+++ test/nuget/ConsoleApplication1/ConsoleApplication1Class.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+#include "ConsoleApplication1Class.h"
+#include "ConsoleApplication1Class.g.cpp"
+
+namespace winrt::ConsoleApplication1::implementation
+{
+    void ConsoleApplication1Class::Test()
+    {
+    }
+}
