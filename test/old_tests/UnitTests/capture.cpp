ede68ed9e78d5ec53110c462fe687be4ea2a5bf7
Raymond Chen <oldnewthing@users.noreply.github.com>
2021-03-23 06:01:15 -0700

    Allow capturing from a raw pointer (#896)

--- test/old_tests/UnitTests/capture.cpp
+++ test/old_tests/UnitTests/capture.cpp
@@ -39,16 +39,26 @@ HRESULT __stdcall CreateCapture(int value, GUID const& iid, void** object) noexc
 
 TEST_CASE("capture")
 {
+    // Capture from global function.
     com_ptr<ICapture> a = capture<ICapture>(CreateCapture, 10);
     REQUIRE(a->GetValue() == 10);
     a = nullptr;
     a.capture(CreateCapture, 20);
     REQUIRE(a->GetValue() == 20);
 
+    // Capture from com_ptr + method.
     auto b = capture<ICapture>(a, &ICapture::CreateMemberCapture, 30);
     REQUIRE(b->GetValue() == 30);
     b = nullptr;
     b.capture(a, &ICapture::CreateMemberCapture, 40);
+    REQUIRE(b->GetValue() == 40);
+
+    // Capture from raw pointer + method.
+    b = nullptr;
+    b = capture<ICapture>(a.get(), &ICapture::CreateMemberCapture, 30);
+    REQUIRE(b->GetValue() == 30);
+    b = nullptr;
+    b.capture(a.get(), &ICapture::CreateMemberCapture, 40);
     REQUIRE(b->GetValue() == 40);
 
     com_ptr<IDispatch> d;
@@ -57,22 +67,35 @@ TEST_CASE("capture")
     REQUIRE_THROWS_AS(d.capture(CreateCapture, 0), hresult_no_interface);
     REQUIRE_THROWS_AS(capture<IDispatch>(a, &ICapture::CreateMemberCapture, 0), hresult_no_interface);
     REQUIRE_THROWS_AS(d.capture(a, &ICapture::CreateMemberCapture, 0), hresult_no_interface);
+    REQUIRE_THROWS_AS(capture<IDispatch>(a.get(), &ICapture::CreateMemberCapture, 0), hresult_no_interface);
+    REQUIRE_THROWS_AS(d.capture(a.get(), &ICapture::CreateMemberCapture, 0), hresult_no_interface);
 }
 
 TEST_CASE("try_capture")
 {
     // Identical to the "capture" test above, just with different
     // error handling.
+
+    // Capture from global function.
     com_ptr<ICapture> a = try_capture<ICapture>(CreateCapture, 10);
     REQUIRE(a->GetValue() == 10);
     a = nullptr;
     REQUIRE(a.try_capture(CreateCapture, 20));
     REQUIRE(a->GetValue() == 20);
 
+    // Capture from com_ptr + method.
     auto b = try_capture<ICapture>(a, &ICapture::CreateMemberCapture, 30);
     REQUIRE(b->GetValue() == 30);
     b = nullptr;
     REQUIRE(b.try_capture(a, &ICapture::CreateMemberCapture, 40));
+    REQUIRE(b->GetValue() == 40);
+
+    // Capture from raw pointer + method.
+    b = nullptr;
+    b = try_capture<ICapture>(a.get(), &ICapture::CreateMemberCapture, 30);
+    REQUIRE(b->GetValue() == 30);
+    b = nullptr;
+    b.try_capture(a.get(), &ICapture::CreateMemberCapture, 40);
     REQUIRE(b->GetValue() == 40);
 
     com_ptr<IDispatch> d;
@@ -81,4 +104,6 @@ TEST_CASE("try_capture")
     REQUIRE(!d.try_capture(CreateCapture, 0));
     REQUIRE(!try_capture<IDispatch>(a, &ICapture::CreateMemberCapture, 0));
     REQUIRE(!d.try_capture(a, &ICapture::CreateMemberCapture, 0));
+    REQUIRE(!try_capture<IDispatch>(a.get(), &ICapture::CreateMemberCapture, 0));
+    REQUIRE(!d.try_capture(a.get(), &ICapture::CreateMemberCapture, 0));
 }
