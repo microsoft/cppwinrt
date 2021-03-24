e72eeb79b62b6d657d5406f0199e755863e72a86
Charles Milette <charles.milette@gmail.com>
2021-03-23 20:44:14 -0400

    Tabs -> spaces

--- test/test_cpp20/ranges.cpp
+++ test/test_cpp20/ranges.cpp
@@ -4,45 +4,45 @@
 
 TEST_CASE("ranges")
 {
-	{
-		// random-access range algorithms
-		auto v = winrt::single_threaded_vector<int>({ 9, 8, 9, 6, 5, 8, 9, 3, 5, 3, 5, 3, 4, 7, 2, 1, 2, 3, 1 });
+    {
+        // random-access range algorithms
+        auto v = winrt::single_threaded_vector<int>({ 9, 8, 9, 6, 5, 8, 9, 3, 5, 3, 5, 3, 4, 7, 2, 1, 2, 3, 1 });
 
-		const bool result = std::ranges::is_heap(v);
+        const bool result = std::ranges::is_heap(v);
 
-		REQUIRE((result == true));
-	}
-	{
-		// bidirectional range views
-		auto v = winrt::single_threaded_vector<int>({ 1, 2, 3 });
+        REQUIRE((result == true));
+    }
+    {
+        // bidirectional range views
+        auto v = winrt::single_threaded_vector<int>({ 1, 2, 3 });
 
-		std::vector<int> result;
-		for (const int i : v | std::views::reverse)
-		{
-			result.push_back(i);
-		}
+        std::vector<int> result;
+        for (const int i : v | std::views::reverse)
+        {
+            result.push_back(i);
+        }
 
-		REQUIRE((result == std::vector{ 3, 2, 1 }));
-	}
-	{
-		// input range algorithms
-		// decay to IIterable is important, we want to test the non-fast iterators.
-		winrt::Windows::Foundation::Collections::IIterable<int> iterable = winrt::single_threaded_vector<int>({ 2, 3, 1 });
+        REQUIRE((result == std::vector{ 3, 2, 1 }));
+    }
+    {
+        // input range algorithms
+        // decay to IIterable is important, we want to test the non-fast iterators.
+        winrt::Windows::Foundation::Collections::IIterable<int> iterable = winrt::single_threaded_vector<int>({ 2, 3, 1 });
 
-		const int result = (std::ranges::max)(iterable);
-		REQUIRE((result == 3));
-	}
-	{
-		// input range views
-		// decay to IIterable is important, we want to test the non-fast iterators.
-		winrt::Windows::Foundation::Collections::IIterable<int> iterable = winrt::single_threaded_vector<int>({ 1, 2, 3 });
+        const int result = (std::ranges::max)(iterable);
+        REQUIRE((result == 3));
+    }
+    {
+        // input range views
+        // decay to IIterable is important, we want to test the non-fast iterators.
+        winrt::Windows::Foundation::Collections::IIterable<int> iterable = winrt::single_threaded_vector<int>({ 1, 2, 3 });
 
-		std::vector<int> result;
-		for (const int i : iterable | std::views::transform([](int i) { return i * 2; }))
-		{
-			result.push_back(i);
-		}
+        std::vector<int> result;
+        for (const int i : iterable | std::views::transform([](int i) { return i * 2; }))
+        {
+            result.push_back(i);
+        }
 
-		REQUIRE((result == std::vector{ 2, 4, 6 }));
-	}
+        REQUIRE((result == std::vector{ 2, 4, 6 }));
+    }
 }
