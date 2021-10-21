029c2978cfd3dd887fd7473c9e9a333e3b64f795
Peter Torr (MSFT) <ptorr@microsoft.com>
2021-10-21 09:43:22 -0700

    Update component_writers.h
    More cleanup based on Raymond's feedback.

--- cppwinrt/component_writers.h
+++ cppwinrt/component_writers.h
@@ -992,12 +992,12 @@ namespace winrt::@::implementation
 // add this file to your project, as any changes you make will be lost.
 //
 // To add a copy of this file to your project:
-//   1. Copy this file from the 'Generated Files' directory into the
-//      directory where you store your other source files (e.g. the project root).
-//   2. Add the copied file to your project. In Visual Studio, you
-//      can use Project -> Add Existing Item.
+//   1. Copy this file from its original location to the location where you store 
+//      your other source files (e.g. the project root). 
+//   2. Add the copied file to your project. In Visual Studio, you can use 
+//      Project -> Add Existing Item.
 //   3. Delete this comment and the 'static_assert' (below) from the copied file.
-//      (Do not modify this file.)
+//      Do not modify the original file.
 //
 // To update an existing file in your project:
 //   1. Copy the relevant changes from this file and merge them into the copy 
