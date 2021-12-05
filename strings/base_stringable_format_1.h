6592bba90271e51f7f75567894ec26227adfc076
Charles Milette <charles.milette@gmail.com>
2021-12-05 16:27:59 -0500

    Keep normal auto

--- strings/base_stringable_format_1.h
+++ strings/base_stringable_format_1.h
@@ -4,6 +4,6 @@ template <>
 struct std::formatter<winrt::Windows::Foundation::IStringable, wchar_t> : std::formatter<winrt::hstring, wchar_t>
 {
     template <typename FormatContext>
-    WINRT_IMPL_AUTO(void) format(winrt::Windows::Foundation::IStringable const& obj, FormatContext& fc);
+    auto format(winrt::Windows::Foundation::IStringable const& obj, FormatContext& fc);
 };
 #endif
