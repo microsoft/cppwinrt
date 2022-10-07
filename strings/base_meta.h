61b054a8c7fdd7b6f0d2d1f4dbfb8283bc9288c8
Charles Milette <charles.milette@gmail.com>
2022-10-07 00:33:52 -0400

    Fix classic COM errors on non-supported compilers (#1194)

--- strings/base_meta.h
+++ strings/base_meta.h
@@ -117,22 +117,33 @@ namespace winrt::impl
         static constexpr auto data{ category_signature<category_t<T>, T>::data };
     };
 
-#if defined(__clang__)
     template <typename T>
-    struct classic_com_guid
+    struct classic_com_guid_error
     {
-#if __has_declspec_attribute(uuid) && defined(WINRT_IMPL_IUNKNOWN_DEFINED)
-        static constexpr guid value{ __uuidof(T) };
-#else
-        static_assert(std::is_void_v<T> /* dependent_false */, "To use classic COM interfaces, you must compile with -fms-extensions and include <unknwn.h> before including C++/WinRT headers.");
+#ifdef __clang__
+#if !__has_declspec_attribute(uuid)
+        static_assert(std::is_void_v<T> /* dependent_false */, "To use classic COM interfaces, you must compile with -fms-extensions.");
+#endif
+
+#ifndef WINRT_IMPL_IUNKNOWN_DEFINED
+        static_assert(std::is_void_v<T> /* dependent_false */, "To use classic COM interfaces, you must include <unknwn.h> before including C++/WinRT headers.");
+#endif
+#else // MSVC won't hit this struct, so we can safely assume everything that isn't Clang isn't supported
+        static_assert(std::is_void_v<T> /* dependent_false */, "Classic COM interfaces are not supported with this compiler.");
 #endif
     };
 
     template <typename T>
-    inline constexpr guid guid_v = classic_com_guid<T>::value;
-#else
-    template <typename T>
+#ifdef __clang__
+#if __has_declspec_attribute(uuid) && defined(WINRT_IMPL_IUNKNOWN_DEFINED)
     inline constexpr guid guid_v{ __uuidof(T) };
+#else
+    inline constexpr guid guid_v = classic_com_guid_error<T>::value;
+#endif
+#elif defined(_MSC_VER)
+    inline constexpr guid guid_v{ __uuidof(T) };
+#else
+    inline constexpr guid guid_v = classic_com_guid_error<T>::value;
 #endif
 
     template <typename T>
