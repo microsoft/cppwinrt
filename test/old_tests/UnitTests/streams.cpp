c0752898b520b8d43c7f7a680a044189b8a59766
Jaiganésh Kumaran <jaiganesh.kumaran@outlook.com>
2022-10-31 21:01:17 +0530

    Added tests

---
+++ test/old_tests/UnitTests/streams.cpp
@@ -0,0 +1,35 @@
+#include "pch.h"
+#include "catch.hpp"
+
+struct stringable : winrt::implements<stringable, winrt::Windows::Foundation::IStringable>
+{
+    winrt::hstring ToString()
+    {
+        return L"a stringable object";
+    }
+};
+
+TEST_CASE("streams")
+{
+    {
+        std::wstringstream ss;
+        winrt::hstring str = L"Hello World";
+        ss << str;
+        REQUIRE(ss.str() == str);
+    }
+
+    {
+        // Support embedded nulls.
+        std::wstringstream ss;
+        winrt::hstring str = L"Hello\0World";
+        ss << str;
+        REQUIRE(ss.str() == str);
+    }
+
+    {
+        std::wstringstream ss;
+        winrt::Windows::Foundation::IStringable obj = winrt::make<stringable>();
+        ss << obj.ToString();
+        REQUIRE(ss.str() == obj.ToString());
+    }
+}
