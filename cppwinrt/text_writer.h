530d383ddf3e5ce63665ff294b701850d6692569
alvinhochun <alvin@alvinhc.com>
2022-11-22 01:59:13 +0800

    Partial GCC compatibility improvements (#1234)

--- cppwinrt/text_writer.h
+++ cppwinrt/text_writer.h
@@ -6,6 +6,7 @@
 #include <sstream>
 #include <string>
 #include <string_view>
+#include <vector>
 
 namespace cppwinrt
 {
--- strings/base_includes.h
+++ strings/base_includes.h
@@ -6,6 +6,7 @@
 #include <charconv>
 #include <chrono>
 #include <cstddef>
+#include <cstring>
 #include <iterator>
 #include <map>
 #include <memory>
@@ -19,6 +20,10 @@
 #include <unordered_map>
 #include <utility>
 #include <vector>
+
+#if __has_include(<version>)
+#include <version>
+#endif
 
 #if __has_include(<windowsnumerics.impl.h>)
 #define WINRT_IMPL_NUMERICS
@@ -50,7 +55,7 @@ namespace winrt::impl
     using suspend_never = std::suspend_never;
 }
 
-#else
+#elif __has_include(<experimental/coroutine>)
 
 #include <experimental/coroutine>
 
@@ -63,4 +68,6 @@ namespace winrt::impl
     using suspend_never = std::experimental::suspend_never;
 }
 
+#else
+#error C++/WinRT requires coroutine support, which is currently missing. Try enabling C++20 in your compiler.
 #endif
