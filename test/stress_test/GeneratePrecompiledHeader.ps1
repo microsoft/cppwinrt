a23bd54750c53e6a2f50f5e3c0d647eee0df2b02
David Machaj <46852402+dmachaj@users.noreply.github.com>
2024-11-20 12:04:17 -0800

    Add a stress test project that includes every single projected SDK header and treats warnings as errors

---
+++ test/stress_test/GeneratePrecompiledHeader.ps1
@@ -0,0 +1,26 @@
+[CmdletBinding()]
+Param
+(
+    [Parameter(Mandatory=$true)]
+    [string]$generatedFilesDir,
+
+    [Parameter(Mandatory=$true)]
+    [string]$cppwinrtHeaderFolder
+)
+
+if (!(Test-Path $generatedFilesDir))
+{
+    mkdir $generatedFilesDir;
+}
+
+$generatedPchContent = "#pragma once`r`n`r`n";
+
+$allHeaders = Get-ChildItem "$cppwinrtHeaderFolder\\Windows.*.h";
+foreach ($header in $allHeaders)
+{
+    $chunks = $header.FullName.Split("\\");
+    $header = $chunks[$chunks.Length - 1];
+    $generatedPchContent += "#include <winrt/$header>`r`n";
+}
+
+Set-Content -Path "$generatedFilesDir\\pch.h" -Value $generatedPchContent -Force -Encoding UTF8
---
+++ test/stress_test/main.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+
+using namespace winrt;
+
+int main()
+{
+}
