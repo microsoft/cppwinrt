71a15b387755e9f404db2928e589904138115f92
David Machaj <46852402+dmachaj@users.noreply.github.com>
2024-11-20 13:48:58 -0800

    Clean back up some after finally detecting the warning Ive been trying to detect

--- test/stress_test/main.cpp
+++ test/stress_test/main.cpp
@@ -9,5 +9,9 @@ int main()
     winrt::Windows::Foundation::Uri uri{ L"https://www.microsoft.com" };
     const auto asString = uri.ToString();
 
+    // Cause the "if constexpr" block to be reachable, possibly triggering a build warning.
+    winrt::Windows::Networking::Sockets::StreamWebSocket socket{ nullptr };
+    socket.Close(1000, L"");
+
     return 0;
 }
