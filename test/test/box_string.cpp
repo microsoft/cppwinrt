176ee159b74838e2f1dabdb8b6a5dadf759c055b
justanotheranonymoususer <justanotheranonymoususer@users.noreply.github.com>
2026-04-03 01:06:44 +0300

    unbox_value_or: Replace param::hstring with hstring

--- test/test/box_string.cpp
+++ test/test/box_string.cpp
@@ -50,4 +50,33 @@ TEST_CASE("box_string")
         auto boxed = winrt::box_value(value);
         REQUIRE(winrt::unbox_value<winrt::hstring>(boxed) == L"");
     }
+
+    // unbox_value_or: default not used (successful unbox)
+    {
+        auto boxed = winrt::box_value(L"boxed");
+        REQUIRE(winrt::unbox_value_or<winrt::hstring>(boxed, L"fallback") == L"boxed");
+        REQUIRE(winrt::unbox_value_or<winrt::hstring>(boxed, winrt::hstring{ L"fallback" }) == L"boxed");
+        REQUIRE(winrt::unbox_value_or<winrt::hstring>(boxed, std::wstring{ L"fallback" }) == L"boxed");
+        REQUIRE(winrt::unbox_value_or<winrt::hstring>(boxed, std::wstring_view{ L"fallback" }) == L"boxed");
+    }
+
+    // unbox_value_or: default used (failed unbox) with each string type
+    {
+        // hstring
+        REQUIRE(winrt::unbox_value_or<winrt::hstring>(nullptr, winrt::hstring{ L"hstring" }) == L"hstring");
+
+        // wchar_t const* (string literal)
+        REQUIRE(winrt::unbox_value_or<winrt::hstring>(nullptr, L"literal") == L"literal");
+
+        // std::wstring
+        REQUIRE(winrt::unbox_value_or<winrt::hstring>(nullptr, std::wstring{ L"wstring" }) == L"wstring");
+
+        // std::wstring_view (null-terminated)
+        REQUIRE(winrt::unbox_value_or<winrt::hstring>(nullptr, std::wstring_view{ L"view" }) == L"view");
+
+        // std::wstring_view (not null-terminated)
+        std::wstring source = L"ABCDE";
+        std::wstring_view fallback(source.data(), 3); // "ABC"
+        REQUIRE(winrt::unbox_value_or<winrt::hstring>(nullptr, fallback) == L"ABC");
+    }
 }
